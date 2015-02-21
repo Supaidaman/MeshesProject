807b2747dd5eba495f2121ed85c9ecb91568f119
Vinícius Costa <viniciuslc476@gmail.com>
2015-02-21 18:05:16 -0200

    Branch com SideCreator
    Ao invés de cada script ter seus métodos separados, todos herdam de
    SideCreator que herda de MonoBehaviour

--- Assets/Scripts/CreateBottom.cs
+++ Assets/Scripts/CreateBottom.cs
@@ -1,101 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-public class CreateBottom: MonoBehaviour{
-
-
-    public List<Vector3> newVertices = new List<Vector3>();
-
-    // The triangles tell Unity how to build each section of the mesh joining
-    // the vertices
-    public List<int> newTriangles = new List<int>();
-
-    // The UV list is unimportant right now but it tells Unity how the texture is
-    // aligned on each polygon
-    public List<Vector2> newUV = new List<Vector2>();
-    int faceCount = 0;
-	// Use this for initialization
-    public Mesh mesh;
-    public MeshFilter mF;
-    //GameObject bottomOb;
-    int count = 0;
-    public int blockWidth; int blockHeight; int blockProf;
-    public bool hasChanged;
-    
-    void Start () 
-    {
-        gameObject.AddComponent<MeshFilter>();
-        gameObject.AddComponent<MeshRenderer>();
-        mesh = GetComponent<MeshFilter>().mesh;
-        create(2, 2, 2, Vector3.zero, 0, gameObject);
-        mesh.Clear();
-
-        mesh.vertices = newVertices.ToArray();
-        Debug.Log("vertices.. :" + newVertices.Count + " uvs...: " + newUV.Count);
-        mesh.triangles = newTriangles.ToArray();
-        //  Debug.Log("vertices.. :" + newVertices.Count + " uvs...: " + newUV.Count );
-        Debug.Log("triangulo" + newTriangles.Count + " uvs...: " + newUV.Count);
-        mesh.uv = newUV.ToArray();
-
-        mesh.Optimize();
-        mesh.RecalculateNormals();
+public class CreateBottom: SideCreator{
 
 
 
-        faceCount = 0;
-        newVertices.Clear();
-        newTriangles.Clear();
-        newUV.Clear();
-        faceCount = 0;
-        Debug.Log("lol");
-	
-    }
-
-    // Update is called once per frame
-    public void Update()
-        
-    {
-        
-    }
-    
-    private void addTriangles()
-    {
-        newTriangles.Add((faceCount * 4));
-        newTriangles.Add((faceCount * 4) + 1);
-        newTriangles.Add((faceCount * 4) + 2);
-        newTriangles.Add((faceCount * 4) + 0);
-        newTriangles.Add((faceCount * 4) + 2);
-        newTriangles.Add((faceCount * 4) + 3);
-
-
-        // para os uvs de cada face...: vejo quem é o topo/fim etc
-        //OU
-        //tento aplicar a ideia do atlas
-        //newUV.Add(new Vector2(0, 1));
-        //newUV.Add(new Vector2(1, 1));
-        //newUV.Add(new Vector2(1, 0));
-        //newUV.Add(new Vector2(0, 0));
-    }
-    //deletar tudo antes de adicionar de novo mas primeiro testo isso...
-
-
-    private void addReverseTriangles()
-    {
-        newTriangles.Add((faceCount * 4));
-        newTriangles.Add((faceCount * 4) + 2);
-        newTriangles.Add((faceCount * 4) + 1);
-        newTriangles.Add((faceCount * 4) + 0);
-        newTriangles.Add((faceCount * 4) + 3);
-        newTriangles.Add((faceCount * 4) + 2);
-
-        //newUV.Add(new Vector2(0, 1));
-        //newUV.Add(new Vector2(1, 1));
-        //newUV.Add(new Vector2(1, 0));
-        //newUV.Add(new Vector2(0, 0));
-    }
-
-
-    public void create( int blockWidth, int blockHeight, int blockProf, Vector3 start, int i, GameObject parent)
+    public override void create( int blockWidth, int blockHeight, int blockProf, Vector3 start, int i, GameObject parent)
     {
         //if (count == 0)
         //{
